a0a62fe246d18b9076900bf48c5edec83434abd0
Stuart Maclean <sm@cmeleadgen.com>
2020-06-11 16:39:19 +0100

    Core added

---
+++ Core/Constants/Generic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Constants
+{
+    public enum Generic
+    {
+        Unspecified = 0,
+        Pending = 5,
+        Active = 10,
+        Deleted = 99
+    }
+}
---
+++ Core/Data/Connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Data
+{
+    public static class Connection
+    {
+        public static string ConnectionString { get; set; }
+    }
+
+}
